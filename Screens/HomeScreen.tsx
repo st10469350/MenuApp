e50675812a171005f56352c14fcce4f6724062cb
st10469350 <ST10469350@vcconnect.edu.za>
2025-11-10 23:41:52 +0200

    colour change in one pf my screens

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -170,3 +170,27 @@ const styles = StyleSheet.create({
   fabAlt: { backgroundColor: "#857f34ff" },
   fabText: { color: "#fdfdf7ff", fontWeight: "900" },
 });
+
+//CODE ATTRIBUTION//
+//PRIMARY SOURCE: IIE Module Manual 2025
+//TITLE: IIE Module Manual 2025
+//AUTHOR: The Independent Institute of Education (Pty) Ltd
+//DATE: 18/10/2025
+//VERSION: First Edition
+//AVAILABLE: https://advtechonline.sharepoint.com/:w:/r/sites/TertiaryStudents/IIE%20Student%20Materials/New%20Student%20Materials%20CAT/WEDE5020/2025/Term%202/WEDE5020MM.docx?d=w2d7589e9374b43de9596ef35d83ebd02&csf=1&web=1&e=Fg8mH2
+
+
+//PRIMARY SOURCE: W3Schools TypeScript Tutorials
+//TITLE: W3Schools - TypeScript Tutorial
+//AUTHOR: W3Schools
+//DATE: 10/11/2025
+//VERSION: Current Online Version
+//AVAILABLE: https://www.w3schools.com/typescript/
+
+//PRIMARY SOURCE: GeeksforGeeks TypeScript Resources
+//TITLE: GeeksforGeeks - TypeScript Programming Resources
+//AUTHOR: GeeksforGeeks
+//DATE: 10/11/2025
+//VERSION: Current Online Version
+//AVAILABLE: https://www.geeksforgeeks.org/typescript-tutorials/
+
--- Screens/WelcomeScreen.tsx
+++ Screens/WelcomeScreen.tsx
@@ -77,7 +77,32 @@ const styles = StyleSheet.create({
 
   // If you have other components
   card: { backgroundColor: "#f7f2f0ff", borderRadius: 16, marginVertical: 8 },
-  text: { color: "#f5e9d7" },
+  text: { color: "#fff7ebff" },
   meta: { color: "#f2f1dbff", fontSize: 12, opacity: 0.7 },
   accent: { color: "#96904cff" },
 });
+
+
+//CODE ATTRIBUTION//
+//PRIMARY SOURCE: IIE Module Manual 2025
+//TITLE: IIE Module Manual 2025
+//AUTHOR: The Independent Institute of Education (Pty) Ltd
+//DATE: 18/10/2025
+//VERSION: First Edition
+//AVAILABLE: https://advtechonline.sharepoint.com/:w:/r/sites/TertiaryStudents/IIE%20Student%20Materials/New%20Student%20Materials%20CAT/WEDE5020/2025/Term%202/WEDE5020MM.docx?d=w2d7589e9374b43de9596ef35d83ebd02&csf=1&web=1&e=Fg8mH2
+
+
+//PRIMARY SOURCE: W3Schools TypeScript Tutorials
+//TITLE: W3Schools - TypeScript Tutorial
+//AUTHOR: W3Schools
+//DATE: 10/11/2025
+//VERSION: Current Online Version
+//AVAILABLE: https://www.w3schools.com/typescript/
+
+//PRIMARY SOURCE: GeeksforGeeks TypeScript Resources
+//TITLE: GeeksforGeeks - TypeScript Programming Resources
+//AUTHOR: GeeksforGeeks
+//DATE: 10/11/2025
+//VERSION: Current Online Version
+//AVAILABLE: https://www.geeksforgeeks.org/typescript-tutorials/
+
